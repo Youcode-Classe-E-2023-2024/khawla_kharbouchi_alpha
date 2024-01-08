641f1c32a789dd8b8f778e141bba6cf83d58e8a8
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-01-08 10:24:28 +0100

    back-end

--- codenotif.js
+++ codenotif.js
@@ -1,6 +1,6 @@
 let data = {"message": "imad added khawla"};
 
-fetch('http://localhost/Alpha/Back_end/start/middleware.php/Pnotif', {
+fetch('http://localhost/khawla_kharbouchi_alpha/Back_end/start/middleware.php/Pnotif', {
   method: 'POST',
   body: JSON.stringify(data), 
   headers: {
